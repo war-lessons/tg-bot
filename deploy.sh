921bdee1df80e71d73f1b43c0fc3a2715597fac6
fix35 <fix35@proton.me>
2022-09-19 09:40:26 +0000

    Show log after deployment

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ cargo build --release
 ssh $DEPLOY_HOST sudo systemctl stop $SYSTEMD_SERVICE
 rsync -P ./target/release/war-lessons-bot $DEPLOY_HOST:$DEPLOY_PATH
 ssh $DEPLOY_HOST sudo systemctl start $SYSTEMD_SERVICE
+ssh $DEPLOY_HOST journalctl --no-hostname -S now -qfu $SYSTEMD_SERVICE
